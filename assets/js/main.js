2ff1395b659c6b4e28c0a79d7d29ec80e5dc17ec
Dominik Helle <339477+GeoDodo@users.noreply.github.com>
2020-11-20 21:13:48 +0100

    inital commit

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+
+!(function($) {
+  "use strict";
+
+  // Header fixed on scroll
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('#header').addClass('header-scrolled');
+    } else {
+      $('#header').removeClass('header-scrolled');
+    }
+  });
+
+  if ($(window).scrollTop() > 100) {
+    $('#header').addClass('header-scrolled');
+  }
+
+  function aos_init() {
+    AOS.init({
+      duration: 1000,
+      once: true
+    });
+  }
+  $(window).on('load', function() {
+    aos_init();
+  });
+
+})(jQuery);
